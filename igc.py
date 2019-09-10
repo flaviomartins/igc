4e0bb81c85f6aa00c6e5041b723a9256867ad430
Flavio Martins <flaviomartins@acm.org>
2019-09-10 12:00:36 +0100

    new graphql responses are missing the list of likers
    downloading locations required logging-in

--- igc.py
+++ igc.py
@@ -29,45 +29,47 @@ if __name__ == '__main__':
         logger.info("VISITING\t%d\t%s", len(visited_users), username)
 
         user = ig.get_user(username, OUTPUTDIR, cache='disk')
-        if user is not None and 'user' in user:
-            u = user['user']
+        if user is not None and 'graphql' in user:
+            u = user['graphql']['user']
             u_id = u['id']
 
-            if 'media' in u:
-                u_media = u['media']
-                for i, media in enumerate(u_media['nodes']):
-                    ig.download_media(media['display_src'], OUTPUTDIR, media['id'])
-                    pmedia = ig.get_media(media['code'], OUTPUTDIR, cache='disk')
+            if 'edge_owner_to_timeline_media' in u:
+                u_media = u['edge_owner_to_timeline_media']
+                for i, media in enumerate(u_media['edges']):
+                    node = media['node']
+                    ig.download_media(node['display_url'], OUTPUTDIR, node['shortcode'])
+                    pmedia = ig.get_media(node['shortcode'], OUTPUTDIR, cache='disk')
 
                     if pmedia is not None:
-                        p = pmedia['media']
+                        p = pmedia['graphql']['shortcode_media']
 
-                        if 'location' in p:
-                            p_loc = p['location']
-                            if p_loc is not None:
-                                if 'id' in p_loc:
-                                    location = ig.get_location(p_loc['id'], OUTPUTDIR, cache='disk')
-                                    if location is not None and 'location' in location:
-                                        loc = location['location']
-                                        if loc['id'] not in visited_locations:
-                                            visited_locations.add(loc['id'])
-                                            logger.info("LOCATION\t%s\t%s", loc['id'], loc['name'])
+                        # if 'location' in p:
+                        #     p_loc = p['location']
+                        #     if p_loc is not None:
+                        #         if 'id' in p_loc:
+                        #             location = ig.get_location(p_loc['id'], OUTPUTDIR, cache='disk')
+                        #             if location is not None and 'location' in location:
+                        #                 loc = location['location']
+                        #                 if loc['id'] not in visited_locations:
+                        #                     visited_locations.add(loc['id'])
+                        #                     logger.info("LOCATION\t%s\t%s", loc['id'], loc['name'])
 
-                    if 'comments' in p:
-                        p_comments = p['comments']
-                        for j, comment in enumerate(p_comments['nodes']):
-                            comment_user = comment['user']['username']
+                    if 'edge_media_to_parent_comment' in p:
+                        p_comments = p['edge_media_to_parent_comment']
+                        for j, comment in enumerate(p_comments['edges']):
+                            node = comment['node']
+                            comment_user = node['owner']['username']
                             if comment_user not in visited_users:
                                 # user_pq.inc_priority(comment_user)
                                 user_pq.append(comment_user)
 
-                    if 'likes' in p:
-                        p_likes = p['likes']
-                        for j, comment in enumerate(p_likes['nodes']):
-                            like_user = comment['user']['username']
-                            if like_user not in visited_users:
-                                # user_pq.inc_priority(like_user)
-                                user_pq.append(like_user)
+                    # if 'likes' in p:
+                    #     p_likes = p['likes']
+                    #     for j, comment in enumerate(p_likes['nodes']):
+                    #         like_user = comment['user']['username']
+                    #         if like_user not in visited_users:
+                    #             # user_pq.inc_priority(like_user)
+                    #             user_pq.append(like_user)
 
             # ig_user = ig.get_ig_user(u_id, OUTPUTDIR, cache='disk')
             #
